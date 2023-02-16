54cc532a7604d4358b618aa96ca95a5bba9a8a56
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-16 15:39:19 -0800

    C is fun

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main + Entry point
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+char name = "Programming is like building a multilingual puzzle";
+gets("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
