0680d555c5c57d36f71577ec24c71413a76f0cc5
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-17 15:23:31 -0800

    Shell variables

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,14 @@
  *
  * Return: 0 (Always)
  */
-int main(void){
+int main(void)
+{
 
 	long numero;
-	
+
 	for (numero = 0; numero < 10; numero++)
 	{
-		putchar((numero%10) + '0');
+		putchar((numero % 10) + '0');
 	}
 	putchar('\n');
 	return (0);
