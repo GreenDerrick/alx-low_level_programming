53639eaa4c1f54ec6dff62c3393cc9ef847fd0ac
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-16 16:28:57 -0800

    C is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
  *Return:0 (Always)
  */
 int main(void)
-{
-	puts('and that piece of art is useful \" - Dora Korpar, 2015\-10\-19 \n');
+{	
+	const name[50];
+	name ="Piece of art:
+	printf(name);
 	return (1);
 }
