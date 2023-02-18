2adff99948f14c2d7df9c966828cd3f237ef1280
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-18 01:36:57 -0800

    Shell variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,12 @@ int main(void)
 {
 	int alpha;
 
-	for (alpha = 0; alpha < 10; alpha++)
+	for (alpha = '0'; alpha <= '9'; alpha++)
 	{
 		putchar(alpha);
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/hh.c
+++ 0x01-variables_if_else_while/hh.c
@@ -8,9 +8,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		putchar(number);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return(0);
