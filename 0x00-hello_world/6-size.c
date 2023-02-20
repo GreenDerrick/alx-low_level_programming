df2974f03a74bbf44dc3ca9cffd06a95a82dfcd4
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-20 07:49:56 -0800

    C is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	long int numbers = sizeof(numbers);
 	long long int numbering = sizeof(numbering);
 	float floating = sizeof(floating);
-	
 	printf("Size of char: %c\n", character);
 	printf("Size of int: %d\n", number);
 	printf("size of long int %ld\n", numbers);
