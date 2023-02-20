f7ec68e9f9a0026252dd6d3ba8d7b24cac4a0fbd
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-20 09:16:23 -0800

    C is fun

---
+++ 0x00-hello_world/intel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - generating assembly code
+ *
+ * Return:0 (Always)
+ */
+int main(void)
+{
+	putchar('1');
+
+	return (0);
+}
--- 0x00-hello_world/intel.s
+++ 0x00-hello_world/intel.s
@@ -1,4 +1,4 @@
-	.file	"main.c"
+	.file	"intel.c"
 	.text
 	.globl	main
 	.type	main, @function
@@ -11,6 +11,8 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	movl	$49, %edi
+	call	putchar@PLT
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
