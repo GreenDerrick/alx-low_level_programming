0ec4adb9a097c8130692a8d57d7e3802bd1118ea
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-20 12:38:54 -0800

    C is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int numbering = sizeof(numbering);
 	float floating = sizeof(floating);
 
-	printf("Size of char: %zu byte(s)\n", character);
-	printf("Size of int: %zu byte(s)\n", number);
-	printf("Size of long int %zu byte(s)\n", numbers);
-	printf("Size of long long int: %zu byte(s)\n", numbering);
-	printf("Size of a float: %zu byte(s)\n", floating);
+	printf("Size of char: %d byte(s)\n", character);
+	printf("Size of int: %d byte(s)\n", number);
+	printf("Size of long int %ld byte(s)\n", numbers);
+	printf("Size of long long int: %lld byte(s)\n", numbering);
+	printf("Size of a float: %f byte(s)\n", floating);
 
 	return (0);
 }
