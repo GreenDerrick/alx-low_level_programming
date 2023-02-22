ff446dca7f22f3469f91e5cd4c63d587febe2164
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-22 12:28:11 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase followed by a new line
+ *
+ * Return:0 (Always)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
