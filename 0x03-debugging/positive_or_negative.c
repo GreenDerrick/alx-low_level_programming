2e09bb60e9d9e1109643754a4e2160b91242c3ba
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-23 12:37:28 -0800

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for positive anad negative numbers
+ * this is the positive negative function
+ * Return: 0 (Always)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
