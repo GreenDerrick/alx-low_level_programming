95c8a912e26da86955921b603f5abc856a2bc16c
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-24 10:35:02 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - this is a short description
+ * a - first  number
+ * b - second number
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (a * b);
+}
