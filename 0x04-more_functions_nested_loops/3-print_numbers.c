567a60b29de40447a4cc8e0dae2c9ef3bbc33f27
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-24 10:56:27 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * print_numbers: this is to print all numbers
+ *
+ * Retun: 0 Always
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
