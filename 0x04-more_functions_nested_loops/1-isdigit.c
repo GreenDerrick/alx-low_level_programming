6ad31c00ad82022523ad352dece04718f7867492
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-24 10:21:23 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,12 @@
  * _isdigit - this is the checker for a number
  *
  * Return: 1 (if number)
+ *
+ * int c - this is type c
  */
 int _isdigit(int c)
 {
-	if(isdigit(c))
+	if (isdigit(c))
 		return (1);
 	else
 		return (0);
