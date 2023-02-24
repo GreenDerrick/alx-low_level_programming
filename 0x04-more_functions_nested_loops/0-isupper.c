2f6463653970789761a668027b86795c965e6219
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-24 06:39:24 -0800

    0-putchar.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main- see if the function will function
  *
  * Return: 0 (Always)
  */
-int main(void)
-{
-	int c = 'a';
 
-	int _isupper(int c)
-	{
-		if (c >= 'A' && c <= 'Z')
-			return (1);
-		else
-			return (0);
-	}
-	return (0);
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -8,22 +8,6 @@
  */
 int main(void)
 {
-	char c;
-
-	c = 'A';
-	printf("%c\n: %d\n" c, _isupper(c);
-	c = 'a';
-	printf("%c\n", c);
-	return (0);
+	return(0);
 	
-	if(_isupper(c))
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar('1');
-		_putchar('\n');
-	} else
-	{
-		_putchar(c);
-	}
 }
