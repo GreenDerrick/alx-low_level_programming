41af644d92dd18c1402a987fd65a497b458d5f5d
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-25 15:41:54 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar((alpha) * 10);
+	}
+	_putchar('\n');
+}
