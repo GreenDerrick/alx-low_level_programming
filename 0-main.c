d924f84d1989ade1b42283b32e96d4b2738804b7
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-25 15:53:58 -0800

    0-putchar.c

---
+++ 0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+ * main - tests functions that print if integer is potive or negative
+ *
+ * Return:0 (Always)
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
