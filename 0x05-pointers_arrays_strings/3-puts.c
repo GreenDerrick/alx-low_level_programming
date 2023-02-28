715855f8fc4a9d3ae1211c47523f06200f8b3894
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-28 06:36:31 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,7 +8,15 @@ void _puts(char *str)
 {
 	int number;
 
-	for (number = 0; str[number] != str['\0']; number++)
+	//for (number = 0; str[number] != str['\0']; number++)
+	//{
+//	}
+	
+	number = 0;
+
+	while (str[number] != str['\0'])
 	{
+		_putchar(str);
+		number++;
 	}
 }
