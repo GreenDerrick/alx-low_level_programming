1c79c7af6a7f6a09880349bca5d2210aa0402ed2
GreenDerrick <greenderrickngugi@gmail.com>
2023-02-28 13:30:11 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string given
+ *
+ * @s: variable to store address
+ */
+void rev_string(char *s)
+{
+	int number;
+	
+	for (number = s['\0']; number != 0; number--)
+	{
+		_putchar(s[number]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 int _strlen(char *s);
+void rev_string(char *s);
 
 #endif
