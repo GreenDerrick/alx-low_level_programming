fba1befba52d9284e0362445d2e479f496ff44b4
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-01 15:04:51 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -26,6 +26,74 @@ int _islower(int c)
 			_putchar('1');
 			_putchar('\n');
 			break;
+		case 'e':
+			_putchar('1');
+			_putchar('\n');
+			break;
+		case 'f':
+			_putchar('1');
+			break;
+		case 'g':
+			_putchar('1');
+			break;
+		case 'h':
+			_putchar('1');
+			break;
+		case 'i':
+			_putchar('1');
+			break;
+		case 'j':
+			_putchar('1');
+			break;
+		case 'k':
+			_putchar('1');
+			break;
+		case 'l':
+			_putchar('1');
+			break;
+		case 'm':
+			_putchar('1');
+			_putchar(']\n');
+			break;
+		case 'n':
+			_putchar('1');
+			break;
+		case 'o':
+			_putchar('1');
+			break;
+		case 'p':
+			_putchar('1');
+			break;
+		case 'q':
+			_putchar('1');
+			break;
+		case 'r':
+			_putchar('1');
+			break;
+		case 's':
+			_putchar('1');
+			break;
+		case 't':
+			_putchar('1');
+			break;
+		case 'u':
+			_putchar('1');
+			break;
+		case 'v':
+			_putchar('1');
+			break;
+		case 'w':
+			_putchar('1');
+			break;
+		case 'x':
+			_putchar('1');
+			break;
+		case 'y':
+			_putchar('1');
+			break;
+		case 'z':
+			_putchar('1');
+			break;
 		default:
 			_putchar('0');
 			_putchar('\n');
