1e00fd2446cdf095094d1c2bff075e2b65bc5f84
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-01 18:15:31 -0800

    4-isalpa.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabets in a sequence
- *
+ *_
  * @c: variable return
  */
 int _isalpha(int c)
 {
 	int alpha;
-	
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		if (c != alpha)
