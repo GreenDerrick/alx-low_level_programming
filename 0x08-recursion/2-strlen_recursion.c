e5eea332109e4e8437dce7461a07a6cbef291661
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-08 12:40:52 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - printing the length of a string
+ *
+ * @s: string to be manipulated
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,5 @@
 int len(char *s);
 int _putchar(char c);
 void _puts_recursion(char *s);
-
+int _strlen_recursion(char *s);
 #endif
