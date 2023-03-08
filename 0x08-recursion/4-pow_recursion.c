1fa47f65429b0c502e85fe21e1c9a6b33933106b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-08 14:32:47 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print x to the power of y
+ * @x: manipulating variable
+ * @y: power of x variable
+ * Return: Always 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x ^ y);
+	return (0);
+}
