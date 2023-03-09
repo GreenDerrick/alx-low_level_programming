76722107337c2492767d5d431f49203587d482e4
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-09 13:32:00 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,31 +1,17 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
+ * _islower - check for lower case characters c
+ * 
+ * @c: variable to be manipulated
  *
- * @c: this is the int ballue
- * @c - this is the int value
- * Return: 1 if lowercase 0 if otherwise
+ * Return : 1 if true and 0 if otherwise
  */
 int _islower(int c)
 {
-	switch (c)
+	while(c <= 'z' && c >= 'a')
 	{
-		case 'o':
-			break;
-		case 'b':
-			break;
-		case 's':
-			break;
-		case 'u':
-			break;
-		case 'n':
-			break;
-		case 'h':
-			break;
-		case 'c':
-			break;
-		default:
-			break;
+		return (1);
 	}
-return (c);
+	return (0);
 }
+
