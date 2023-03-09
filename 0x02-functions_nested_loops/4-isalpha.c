a30ccd531e5548ef2207665d504537fd2b62ecd8
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-09 14:07:26 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,16 @@
  * _isalpha - checks for alphabets in a sequence
  *_
  * @c: variable return
+ *
+ * Return: 1 if lowercase or lowercase and 0 if otherwise
  */
 int _isalpha(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else if(c >= 'A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
