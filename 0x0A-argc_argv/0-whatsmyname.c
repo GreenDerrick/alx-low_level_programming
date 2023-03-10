89a5ffd7fb04d1d2ee72e266f46cad7eb4360e41
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-10 13:48:47 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,10 +12,10 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		_putchar(argv[i]);
 	}
 	return (0);
 }
