94972c08ed2c2410cb3ec8ddc14c40f591e2d070
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-12 10:49:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,8 @@
 /**
  * main - contains the *argv[] and argc
  *
- * @argv - this is the argument vector, it return the pointer to the arguments
- * @argc - thi is the argument count, it will return the number of arguments that has been passed
+ * @argv: this is the argument vector, it return the pointer to the arguments
+ * @argc: thi is the argument count, it will return the number of arguments that has been passed
  * Return: 0 Always
  */
 
