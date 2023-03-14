702bcf7562ad3e0ca2ebaac872909dcbd19caa1b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-14 10:49:00 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strdup - function for duplicating the string
+ *
+ * @str: string to be duplicated
+ * Return: NULL if insufficient memory
+ */
+
+char *_strdup(char *str)
+{
+	str = malloc(sizeof(char));
+	
+	if (str == NULL)
+		return (NULL);
+
+	return (str);
+}
