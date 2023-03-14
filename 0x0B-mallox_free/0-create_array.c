45f88b078ebac42ddd9c4356c85e68c9a5a68683
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-14 09:54:45 -0700

    0-create_array.c

--- 0x0B-mallox_free/0-create_array.c
+++ 0x0B-mallox_free/0-create_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * create_array - function that return size and c
+ *
+ * @size: parameter for int value
+ * @c: parameter for character value
+ *
+ * Return: 0 if Null else Return to aaray
+ */
 
 char *create_array(unsigned int size, char c)
 {
@@ -8,11 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	p = malloc(sizeof(char) * size);
 
-	if(p == NULL)
+	if (p == NULL)
 		return (0);
 
 	for (i = 0; i <= size; i++)
 		c = p[i]
-	
 }
 
