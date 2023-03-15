ad919eb0e8e7f17b6941ab0c5c3b2695340dd966
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:01:17 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - string concactenate
+ *
+ * @dest: destination variable
+ * @src: source variable
+ *
+ * Return: 0 if false 1 if true
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
