8c27a3234cc17ccdb649d2afe439b1b457a7f822
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 14:32:05 -0700

    libmy.a

---
+++ 0x09-static_libraries/9-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncpy - string copy function
+ *
+ * @dest: destination
+ * @src: source
+ * @n: int n
+ *
+ * Return: 1
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (0);
+}
