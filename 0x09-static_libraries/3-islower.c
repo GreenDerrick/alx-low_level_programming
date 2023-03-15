123765f03e1c8378125f879ed554e8f9653572e2
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:32:45 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checks for lower characters
+ *
+ * @c: characeter to be mainpulated
+ * Return: 0 if false 1 if true
+ */
+
+int _islower(int c)
+{
+	return (0);
+}
