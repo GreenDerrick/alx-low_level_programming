b13128654927174b0329734e751309c400f6ef2b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:16:47 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strncat -  string concatenate
+ *
+ * @dest: destination
+ * @src: source of string
+ * @n: integer n
+ *
+ * Return: 1 if true and 0 if false
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (0);
+}
