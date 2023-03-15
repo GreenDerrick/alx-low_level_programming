7f7e9f66e89ed2672ee346fed11d8d97ee863406
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:45:48 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strspn - strspn function
+ *
+ * @s: variable s
+ * @accept: pointer accept
+ *
+ * Return: 0 if false 1 if true
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
