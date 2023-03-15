4dea89477a321dffe2bcc52e32ae09056599595a
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 02:56:04 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,10 +11,14 @@
  * Return: 0 if no digits are passed
  */
 
-int main(int argc, __attribute__(unused)char **argv)
+int main(int argc, char **argv)
 {
+	int i;
+	
+	i = 1;
 
-	if (argc == 1)
+
+	if (argc <= i)
 	{
 		printf("0\n");
 	}
