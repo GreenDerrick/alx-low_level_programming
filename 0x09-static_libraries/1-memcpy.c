9d963bb8a72ee76eda130b6534760c5b41ff2414
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:12:50 -0700

    mylib.a

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memcpy - copy memmory
+ *
+ * @dest: destination
+ * @src: destination
+ * @n: integer n
+ *
+ * Return: 0 if false 1 if true
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
