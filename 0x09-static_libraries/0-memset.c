65970a089400d2cd457a1facb8d7df07ce7863f7
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 12:56:04 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - function to set memory to *s b and n
+ *
+ * @s: pointer s
+ * @b: character s
+ * @n: interfer n
+ *
+ * Return: 0 if false 1 if true
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
