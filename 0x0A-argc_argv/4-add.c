35c36fbe12759bc46f4d72e3b8b35bae3ff61f22
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 02:52:02 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,17 @@
  * @argc: argument integer variable
  * @argv: argument vector holds contents of the argument
  *
- * Return : 0 if no digits are passed
+ * Return: 0 if no digits are passed
  */
 
 int main(int argc, char **argv)
 {
 	int i;
-	int add;
+	long int add;
 
-	i = 4;
-
-	while (argc <= i)
+	if (argc == 1)
 	{
-		add = atoi(argv[1]) + atoi(argv[2]) + atoi(argv[3]) + atoi(argv[4]);
-		printf("%d\n", add);
-		i++;
+		printf("0\n");
 	}
 	return (0);
 }
