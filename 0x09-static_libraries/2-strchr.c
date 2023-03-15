dfa69e7afe2ef2742fd90a9e902729f5d983475d
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:22:53 -0700

    mylib.a

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strchr - function for strchr
+ *
+ * @s: pointer s
+ * @c: character c
+ *
+ * Return: 0 if false 1 if true
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (0);
+}
