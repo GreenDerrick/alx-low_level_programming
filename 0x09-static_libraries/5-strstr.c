69c1c03b1337fdf0065dacc9435f7b3e6ed73a3b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:55:58 -0700

    libmy.a

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strstr - checks if string 2 appears in string 1
+ *
+ * @haystack: variable string 1
+ * @needle: variable to check
+ *
+ * Return: 0 if false 1 if true
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
