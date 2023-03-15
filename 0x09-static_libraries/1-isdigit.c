233d83fa146eeaf658a56fadb6f662bd74fcb33c
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:08:45 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - checks if integer c is really a digit
+ *
+ * @c: integer to be manipulated
+ *
+ * Return: 0 if false 1 if true
+ */
+
+int _isdigit(int c)
+{
+	return (0);
+}
