ef64f2e8ac82b09ddd71b5f5a5086c05a9303297
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:40:21 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - string compute
+ *
+ * @s1: pointer s1
+ * @s2: pointer s2
+ *
+ * Return: 0 when false 1 when true
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
