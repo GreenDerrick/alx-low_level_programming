f50f81322a56a78277ce28f40c46276b0867a1f3
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:48:38 -0700

    libmy.a

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - check if c is an alphabet
+ *
+ * @c: character to be manipulated
+ *
+ * Return: 0 if false 1 if true
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
