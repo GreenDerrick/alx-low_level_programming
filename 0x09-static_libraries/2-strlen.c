bf0617515d44b70eb7d47311ddca264e0ebdc235
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-15 13:26:24 -0700

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen -  check for string length
+ *
+ * @s: pointer s to be manipulated
+ *
+ * Return: 0 if false 1 if succesful
+ */
+
+int _strlen(char *s)
+{
+	return (0);
+}
