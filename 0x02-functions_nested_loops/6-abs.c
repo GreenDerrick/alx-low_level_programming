e5ddfbe5cffb5998efc30efcf1a2ce6fa9a8c4d8
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-19 11:59:25 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function to process the absolute value
+ *
+ * Return: 0 always
+ */
+
+int _abs(int)
+{
+	int i;
+	int val;
+
+	if (i < 0)
+	{
+		val = i * (-1);
+		return (val);
+	}
+	return (0);
+}		
