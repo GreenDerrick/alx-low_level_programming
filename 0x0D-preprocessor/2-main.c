a50e7ab19b6050ceeb54e6cfbe76cd1fceb9272b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-20 11:56:56 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main -  main function to print where the file was compiled from
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
