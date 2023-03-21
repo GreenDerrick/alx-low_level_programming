3a0a5593c9d0df2234b67825e481dd9f4a4486e7
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-21 12:11:49 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - structure name
+ *
+ * @name: name of dog
+ * @age: age of poppy
+ * @owner: owner of poppy
+ *
+ * Description: thsi is the longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
