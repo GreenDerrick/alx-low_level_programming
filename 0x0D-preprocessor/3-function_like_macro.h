3a5901ac527a61e0e702b7feac8113585db1881e
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-20 18:42:30 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,10 @@
-#ifndef ABS
+#ifndef ABS_H
+#define ABS_H
+
+/**
+ * ABS - function to define the absolute value
+ *
+ * @x: integer to be manipulated
+ */
 #define ABS(x) ((x) < (0) ? (x * -1) : (x))
 #endif
