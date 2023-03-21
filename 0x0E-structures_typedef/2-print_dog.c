844d08c301c80e13df9bbf80a69a24861cb100f0
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-21 16:01:57 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog -  print contents in a structure
+ *
+ * @struct dog: struct of type dog
+ * @d: pointer value struct type
+ */
+void print_dog(struct dog *d)
+{
+	d = malloc(sizeof(struct dog));
+	
+	if (d == NULL)
+		printf("");
+	if(d.name == NULL)
+		printf(nil);
+}
