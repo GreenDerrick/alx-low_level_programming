578625c4a64d17654e9573fd1feb619e8dbfcac4
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-20 18:37:46 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,8 +1,10 @@
-#ifndef SUM 
+#ifndef SUM
 #define SUM
 /**
  * File: 4-sum.h
  * use: defines a function that adds two numbers
+ * @x: parameter c
+ * @y: parameter y
  */
 
 #define SUM(x, y) ((x) + (y))
