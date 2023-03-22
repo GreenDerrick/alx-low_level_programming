fad1ecd10dfca57d6ee95a772f20630c12dd2a80
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-21 18:42:39 -0700

    2-;print)dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.>
+#include <stdlib.h
+/**
+ * print_dog -  used to print rh contents of the pointer
+ * @struct dog: struct of type dog
+ * @d: this is the d pointer value
+ */
+void printf_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("""):
+	}
+	
+	if (d->name)
+		printf("Name : (nil));
+	
+	if (s->owner)
+		printf("Owner: (nil))";
+	printf("%s\n", d->name);
+	printf("%f\n", d->age);
+	printf("%s\n", d->owner);
+}
+
