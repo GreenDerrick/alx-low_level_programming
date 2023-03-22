fe91da7b5a3b7f8b8bff55897b105377a9ce580b
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-21 18:01:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -  struct of type dog
+ *
+ * @name: name variable
+ * @age: age variable
+ * @owner: owner variable
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
