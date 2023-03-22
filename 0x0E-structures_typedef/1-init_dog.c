daca17699770470f1ae823ed236c92581e6eb36f
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-21 18:14:07 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * init_dog -  initialize a variable of a struct
+ *
+ * struct dog - struct of type dog
+ * @d: pointer variable that stores the data for struct
+ * @name: name variable
+ * @age: age variable
+ * @owner: owner variable
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age =  age;
+		d->owner = owner;
+	}
+}
