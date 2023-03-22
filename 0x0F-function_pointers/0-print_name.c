e1a820f9493fa0e8a42ac4b45ce27653348409f3
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-22 16:19:38 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers_h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_name - function to print the name variables
+ *
+ * @name: variable to be printed
+ * @f: function pointer name
+ * Return: No return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		exit (1);
+	}
+
+	f(name);
+}
