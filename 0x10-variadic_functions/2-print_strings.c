1241e537beaccd98932b7f775319c347a098ba5a
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-23 16:33:13 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_strings - function to print all strings passed in an argument
+ *
+ * @separator: character pointer
+ * @n: integer unsigned constant variable
+ * Return: nothing its of return value void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list(strings);
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(strings, char *) == NULL)
+		{
+			printf("(nil)");
+		}
+
+		if (separator == NULL)
+		{
+			continue;
+		}
+
+		printf("%c", va_arg(strings, int));
+
+	}
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/strings_main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
