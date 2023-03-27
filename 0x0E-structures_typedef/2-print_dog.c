ca0f73e4c90d03eec93e7c329390a8717aaa0bca
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-27 15:39:06 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,42 +10,32 @@
 
 void print_dog(struct dog *d)
 {
-	d =  malloc(sizeof(struct dog));
 
 	if (d != NULL)
 	{
-	
-
-		if (d->name == NULL)
+		if (d -> name == NULL)
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
-		else
+		else 
 		{
 			printf("Name: %s\n", d->name);
 		}
-
-		if (d->age < 0)
+		if (d -> age < 0)
 		{
-			printf("Age: (nil)");
 		}
 		else
 		{
 			printf("Age: %f\n", d->age);
 		}
 
-		if (d->owner == NULL)
+		if (d -> owner == NULL)
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
-			printf("Owner: %s\n", d->owner);
+			printf("Owner: %s\n",  d -> owner);
 		}
 	}
-	else
-	{
-		printf("(nil");
-	}
-
 }
