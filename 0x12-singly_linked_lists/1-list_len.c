f71999458804dbfd93741e18bde69af5c0e7c182
GreenDerrick <greenderrickngugi@gmail.com>
2023-03-30 15:58:52 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * list_len - function that returns a count of number of elements
+ *
+ * @h: variable to the struct
+ * Return: count
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
