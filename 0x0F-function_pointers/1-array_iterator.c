eb152ccbe4d53c41b375b815e12611e6e75865e2
GreenDerrick <greenderrickngugi@gmail.com>
2023-04-20 04:41:36 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - will prints array accordin to the size specified
+ *
+ * @array: pointer variable
+ * @size: size of array to be printed
+ * @action: function pointer
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (i < 0)
+		exit(1);
+	while (i <= size)
+	{	
+		action(array[i]);
+		i++;
+	}
+}
