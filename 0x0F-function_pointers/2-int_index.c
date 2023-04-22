95bc7508546c22392244c7dd35dc06765e06ce81
GreenDerrick <greenderrickngugi@gmail.com>
2023-04-22 05:49:05 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
-  if (array == NULL || size <= 0 || cmp == NULL)
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-  
-	for (index = 0; index < size; index++)
+
+	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[index]))
+		if (cmp(array[i]))
 			return (i);
-  }
+	}
 	return (-1);
 }
