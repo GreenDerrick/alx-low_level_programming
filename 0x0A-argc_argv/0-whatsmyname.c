e05b433dfee702708778f8156e5a08818ce24e68
Green <greenderrickngugi@gmail.com>
2023-06-09 11:57:41 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * @argc: argument counter, holds number of arguments
+ * @argv: argument vector, holds arguments in the function
+ * Return: 0 always
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	printf("%s\n", argv[i]);
+	return (0);
+}
