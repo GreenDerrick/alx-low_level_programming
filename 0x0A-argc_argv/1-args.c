133b073c3ca553447e0c6bbda828b87cd4d14d6e
Green <greenderrickngugi@gmail.com>
2023-06-09 12:20:11 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument counter, holds the number of arguments passed
+ * @argv: argument vector, holds the value of the arguments
+ * Return: 0 always
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
