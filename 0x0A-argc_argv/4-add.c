62101c2e97fd641f19a872d499b069a4f09df028
Green <greenderrickngugi@gmail.com>
2023-06-09 14:09:48 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 always
+ */
+int main(int argc, char **argv)
+{
+	int add;
+	int i;
+
+	add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if ((atoi(argv[i])) >= 0 && (atoi(argv[i])) <= 1000000)
+		{
+			add = add + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
