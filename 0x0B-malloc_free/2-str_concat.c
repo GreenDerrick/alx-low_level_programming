2e7c483955553fd1f8b1de70b62139e10c54b02a
Green <greenderrickngugi@gmail.com>
2023-06-13 18:44:07 -0700

    combine

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: variable to be manipulated
+ * @s2: variable to be manipulated
+ * Return: string concatenated
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int a;
+	int len;
+	int len1;
+	int total;
+	char *ptr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	len += 1;
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		len1 = i;
+	}
+	len1 += 1;
+	total = (len + len1) - 1;
+
+	ptr = malloc(sizeof(char) * total);
+
+	for (a = 0; ptr[a] != '\0'; a++)
+	{
+		while (s1[a] != '\0')
+		{
+			_putchar(s1[a]);
+		}
+		while (s2[a] != '\0')
+		{
+			_putchar(s2[a]);
+		}
+	}
+	return (ptr);
+}
