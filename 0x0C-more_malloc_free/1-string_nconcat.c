944f763179a0c935b84bb9f8e822762e17a8e164
Green <greenderrickngugi@gmail.com>
2023-06-15 14:22:36 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	k = j = 0;
 	if (s1 == NULL || s2 == NULL)
-		return ("");
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		len =  i + 1;
