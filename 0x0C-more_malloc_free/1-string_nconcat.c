b23c463467781b6943fc57875af87f1eadfa23bd
Green <greenderrickngugi@gmail.com>
2023-06-15 14:02:35 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * string_nconcat - concatenating strings
+ * @s1: variable to be manipulated
+ * @s2: variable to be manipulated
+ * @n: variable to be manipulated
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, len, len1, total, j, k;
+	char *ptr;
+
+	k = j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	len += 1;
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		len1 = i;
+	}
+	len1 += 1;
+	total = (len + len1) + 1;
+	printf("%d\n", total);
+	ptr = malloc(sizeof(char) * total);
+
+	if (ptr == NULL)
+		return (NULL);
+	while (s1[k] != '\0')
+	{
+		ptr[k] = s1[k];
+		k++;
+	}
+	while (s2[j] != s2[n])
+	{
+		ptr[k] = s2[j];
+		j++;
+		k++;
+	}
+	return (ptr);
+}
