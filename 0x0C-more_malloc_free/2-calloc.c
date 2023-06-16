638ed215302954e218b2ac9dc0e3a3b51b786887
Green <greenderrickngugi@gmail.com>
2023-06-15 19:26:16 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,18 +3,23 @@
 #include "main.h"
 /**
  * _calloc - function to alocate memory to an array
- * @nmemb - variable
+ * @nmemb: variable
  * @size: size of the memory
  * Return: pointer to the memory allocated
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
+	int i;
 
 	if (nmemb == 0 && size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i <= size; i++)
+	{
+		ptr[i] = i;
+	}
 	return (ptr);
 }
