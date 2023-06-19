e6113e86d9c9eb1f921e921451d9da9a8ae6ac8e
Green <greenderrickngugi@gmail.com>
2023-06-19 12:27:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct dog - structures
  * @name: variable
  * @age: variable
  * @owner: variable
  *
- * Description - this is a struct to contain dogs details
+ * Description: this is a struct to contain dogs details
  */
 struct dog
 {
