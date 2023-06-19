4461430e676af1ef883fa7707f9fd9f9e7329f40
Green <greenderrickngugi@gmail.com>
2023-06-19 12:01:39 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structures
+ * @name: variable
+ * @age: variable
+ * @owner: variable
+ *
+ * Description - this is a struct to contain dogs details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
