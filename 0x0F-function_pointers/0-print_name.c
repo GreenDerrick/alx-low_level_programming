0af236bf2c57485dd8d8e20cfd5dc14382dd6bdd
Green <greenderrickngugi@gmail.com>
2023-06-21 18:41:45 -0700

    check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include "function_pointers.h"
-
 /**
  * print_name - function that prints a name from a function pointer
  * @name: variable to be manipulated
