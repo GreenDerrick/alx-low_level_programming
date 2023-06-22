ee61659a5d37a1091a0758f2c05d0c83236ff100
Green <greenderrickngugi@gmail.com>
2023-06-21 19:20:55 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,9 +3,9 @@
 #include "function_pointers.h"
 /**
  * array_iterator - function that gives a paramerter on each array element
- * @array - variable to be manipulated
+ * @array: variable to be manipulated
  * @size: size of the array
- * @action function poitner
+ * @action: function poitner
  * Return: nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
