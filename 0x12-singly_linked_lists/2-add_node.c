c14f4f8f5739290aad2223c90ff36e291b9ac72f
Green <greenderrickngugi@gmail.com>
2023-06-30 15:24:26 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h.
+#include <stddef.h>
+
+/**
+ * add_node - funciton to add a new node to the beginning of a list
+ * @head: header pointing to first element
+ * @str: string to be copied to fitsr
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
+
