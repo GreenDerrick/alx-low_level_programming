c709d01a2bb7c1c93ca3ca9a0d8f3a0551ac21ad
Green <greenderrickngugi@gmail.com>
2023-07-01 12:21:43 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include "lists.h"
 /**
  * run_before - function that runs before the main function
  * Return: nothing
  */
 void run_before(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
