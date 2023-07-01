bc6fb0f780be28cbc488dfb36d1b7f673dc0e6ab
Green <greenderrickngugi@gmail.com>
2023-07-01 09:09:19 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+	{
+		printf("No data");
+		exit(1);
+	}
 	while (new_node != NULL)
 	{
 		new_node->str = strdup(str);
