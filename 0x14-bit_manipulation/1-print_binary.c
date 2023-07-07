df7a92e287c451915fc67a4eb17a2036a4595883
Green <greenderrickngugi@gmail.com>
2023-07-07 08:45:36 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,10 +10,27 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
-	int binary;
+	int i, a, binary;
 
-	for (i = sizeof(int); i >= 0; i--)
+	if (n >= 0 && n <= 1)
+		a = 0;
+	else if (n >= 2 && n <= 3)
+		a = 1;
+	else if (n >= 4 && n <= 7)
+		a = 2;
+	else if (n >= 8 && n <= 15)
+		a = 3;
+	else if (n >= 16 && n <= 31)
+		a = 4;
+	else if (n >= 32 && n <= 63)
+		a = 5;
+	else if (n >= 64 && n <= 127)
+		a = 6;
+	else if (n >= 128 && n <= 255)
+		a = 7;
+	else
+		a = 10;
+	for (i = a; i >= 0; i--)
 	{
 		binary = (n >> i) & 1;
 		_putchar(binary + '0');
