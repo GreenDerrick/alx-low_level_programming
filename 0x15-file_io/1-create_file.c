3b91b0a6eb166ac4e6a066219c1883703b1ece70
Green <greenderrickngugi@gmail.com>
2023-07-14 12:32:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[count];)
+		for (count = 0; text_content[count];)
 			count++;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wri = write(fd, text_content, count);
+	wri = write(file, text_content, count);
 
 	if (file == -1 || wri == -1)
 		return (-1);
