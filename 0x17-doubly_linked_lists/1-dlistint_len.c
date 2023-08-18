55cc9aa31943d7673408bf0de1d9de80a8a0b170
Green <greenderrickngugi@gmail.com>
2023-08-18 07:49:44 -0700

    print number of elemetns in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * dlistint_len - function that return the number of elemtns in a list
+ * @h: variable name of the struct "dlistint"
+ * Return: number of elements in a list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	if (h == NULL)
+	{
+		printf("No memory allocated");
+		exit(1);
+	}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
