9e9280fdf68954a2f498d158c87315fe8a0384de
Green <greenderrickngugi@gmail.com>
2023-08-18 08:54:37 -0700

    adding a node to the top of the list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function to add a new node at the begining of the list
+ * @head: variable name of the dlistint
+ * @n: constant variable to be added
+ * Return: address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		exit(1);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (*head);
+	free(new);
+}
+
