da4a543e31bbbbd5ee950f72d28945b2cabae809
Green <greenderrickngugi@gmail.com>
2023-09-14 14:04:03 -0700

    free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp0 = head;
+	dlistint_t *tmp = head;
 	dlistint_t *tmp2 = NULL;
 
 	while (tmp)
 	{
-		tmp2 = tmp0->next;
-		free(tmp0);
-		tmp0 = tmp2;
+		tmp2 = tmp->next;
+		free(tmp);
+		tmp = tmp2;
 	}
 }
